758924cd9d54c25d31f1ab5aa0b0c32c8b9e3af0
gersonsena <chgsena@gmail.com>
2018-06-10 12:51:10 -0300

    Teste Inicial Módulo PZEM
    Este é o primeiro teste a ser feito. Ele funciona corretamente tanto em ESP8266 quanto em Arduino. Se ainda assim não se comunicar, o problema pode ser a porta escolhida (não compatível, no caso do ESP), não ter feito a alteração necessária na saída do PZEM (no caso do ESP também, pois no Arduino não precisa), ou não ter ligado o sinal de tensão à entrada do PZEM. Fora essas hipóteses ou problemas de alimentação (de novo o ESP8266) o código vai funcionar.

---
+++ TestePZEM004T.ino
@@ -0,0 +1,36 @@
+#include <PZEM004T.h>
+ 
+PZEM004T pzem(5, 4);  // RX,TX
+IPAddress ip(192,168,1,1);
+
+bool pzemrdy = false;
+ 
+void setup() {
+  Serial.begin(9600);
+
+  while (!pzemrdy) {
+      Serial.println("Connecting to PZEM...");
+      pzemrdy = pzem.setAddress(ip);
+      delay(1000);
+   }
+}
+ 
+void loop() {
+ 
+  delay(5000);
+  
+  float v = pzem.voltage(ip);
+  if (v < 0.0) v = 0.0;
+  Serial.print(v);Serial.print("V; ");
+ 
+  float i = pzem.current(ip);
+  if(i >= 0.0){ Serial.print(i);Serial.print("A; "); }
+  
+  float p = pzem.power(ip);
+  if(p >= 0.0){ Serial.print(p);Serial.print("W; "); }
+  
+  float e = pzem.energy(ip);
+  if(e >= 0.0){ Serial.print(e);Serial.print("Wh; "); }
+ 
+  Serial.println();
+}
